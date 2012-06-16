9e39a96addd624f2dbd22f812d67907589a862e7
ndobromirov <ndobromirov@gmail.com>
2012-06-16 20:03:08 +0000

    - added initial crud operations.

---
+++ site/protected/views/product/view.php
@@ -0,0 +1,33 @@
+<?php
+$this->breadcrumbs=array(
+	'Products'=>array('index'),
+	$model->name,
+);
+
+$this->menu=array(
+	array('label'=>'List Product', 'url'=>array('index')),
+	array('label'=>'Create Product', 'url'=>array('create')),
+	array('label'=>'Update Product', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Product', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Product', 'url'=>array('admin')),
+);
+?>
+
+<h1>View Product #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'category_id',
+		'manifacturer_id',
+		'measure_id',
+		'name',
+		'delivery_prize',
+		'sell_prize',
+		'current_quantity',
+		'minimum_quantity',
+		'create_time',
+		'update_time',
+	),
+)); ?>
