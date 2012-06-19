f74323841d0de860dbf9168e0ee973a274ebbdc9
ndobromirov@gmail.com <ndobromirov@gmail.com@1bd86575-0b46-533d-27c8-11066aea234b>
2012-06-19 21:52:34 +0000

    - finished with the final tweaks.

--- trunk/site/protected/views/site/index.php
+++ trunk/site/protected/views/site/index.php
@@ -2,15 +2,14 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>This is application for managing the work-flow of a mini-market store.</p>
+<p>It si developed as a course project at FMI 2012 summer semester by Nikolay Dobromirov.</p>
+<p>
+    Developed functionality for managment of:
+    <ul>
+        <li>Product manifacturers</li>
+        <li>Poduct categories</li>
+        <li>Products</li>
+        <li>Orders (Supply &amp; Delivery)</li>
+    </ul>
+</p>
