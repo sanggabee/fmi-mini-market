7d4f70ba705f3b615f9c1c4596d38f47cf3a1009
ndobromirov@gmail.com <ndobromirov@gmail.com@1bd86575-0b46-533d-27c8-11066aea234b>
2012-06-19 20:13:05 +0000

    - finished the glue widget

--- trunk/site/protected/views/product/_form.php
+++ trunk/site/protected/views/product/_form.php
@@ -32,31 +32,31 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->textField($model,'name',array('size'=>20,'maxlength'=>100)); ?>
 		<?php echo $form->error($model,'name'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'delivery_prize'); ?>
-		<?php echo $form->textField($model,'delivery_prize',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->textField($model,'delivery_prize',array('size'=>20,'maxlength'=>10)); ?>
 		<?php echo $form->error($model,'delivery_prize'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'sell_prize'); ?>
-		<?php echo $form->textField($model,'sell_prize',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->textField($model,'sell_prize',array('size'=>20,'maxlength'=>10)); ?>
 		<?php echo $form->error($model,'sell_prize'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'current_quantity'); ?>
-		<?php echo $form->textField($model,'current_quantity',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->textField($model,'current_quantity',array('size'=>20,'maxlength'=>10)); ?>
 		<?php echo $form->error($model,'current_quantity'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'minimum_quantity'); ?>
-		<?php echo $form->textField($model,'minimum_quantity',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->textField($model,'minimum_quantity',array('size'=>20,'maxlength'=>10)); ?>
 		<?php echo $form->error($model,'minimum_quantity'); ?>
 	</div>
 
